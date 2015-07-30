398304e8b48e4bd53203f5234db7535a59900cb1
mhoogen <m.hoogendoorn@vu.nl>
2015-07-30 15:56:14 -0400

    directory settings

--- aggregate_caleb_style.py
+++ aggregate_caleb_style.py
@@ -2,5 +2,5 @@ from data_processing.process_data import createProcessedFiles
 from util_ import in_out as io
 
 c = createProcessedFiles()
-[attr, values] = c.read_files_and_calculate_attributes('/scratch/mimic_dump/mimic2v26_rohit_no_hold.csv', '../../data/export_file_caleb.csv', 0)
+[attr, values] = c.read_files_and_calculate_attributes('/scratch/mimic_dump/mimic2v26_rohit_no_hold.csv', '../../data/caleb/export_file_caleb.csv', 0)
 # [attr, values] = c.read_files_and_calculate_attributes('/Users/markhoogendoorn/Documents/Mijn Research Documenten/MIMIC-II/V3.0_export/temp.csv', '/Users/markhoogendoorn/Documents/Mijn Research Documenten/MIMIC-II/V3.0_export/processed/caleb/export_file_caleb.csv', 0)
--- learn_caleb_style.py
+++ learn_caleb_style.py
@@ -1,7 +1,7 @@
 from learning import learn
 from util_ import util
 
-in_dir = '/Users/markhoogendoorn/Documents/Mijn Research Documenten/MIMIC-II/V3.0_export/processed/caleb/'
+in_dir = '../../data/caleb/'
 now = util.get_current_datetime()
-out_dir = '/Users/markhoogendoorn/Documents/Mijn Research Documenten/MIMIC-II/V3.0_export/learning_results/caleb/' + now
+out_dir = '../../data/learning_results/caleb/' + now
 learn.execute(in_dir, out_dir, 'id', 'label', 'day', 1, ['LR'], True, False, '')
