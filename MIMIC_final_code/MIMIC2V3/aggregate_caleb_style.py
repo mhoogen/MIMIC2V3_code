8470066ca737aacd43e90df5c17b6f77b04f14c6
alihassouni <ali_e27@hotmail.com>
2016-05-20 10:37:20 +0200

    Update aggregate_caleb_style.py

--- MIMIC_final_code/MIMIC2V3/aggregate_caleb_style.py
+++ MIMIC_final_code/MIMIC2V3/aggregate_caleb_style.py
@@ -3,6 +3,4 @@ from util_ import in_out as io
 
 c = createProcessedFiles()
 
-[attr, values] = c.read_files_and_calculate_attributes('C:/Users/ali_e_000/Desktop/data_mimic_mark/outcome_result.csv', 'C:/Users/ali_e_000/Desktop/Research Paper/knn/500000_caleb_new.csv', 0)
-# [attr, values] = c.read_files_and_calculate_attributes('/Users/markhoogendoorn/Documents/Mijn Research Documenten/MIMIC-II/V3.0_export/temp.csv', '/Users/markhoogendoorn/Documents/Mijn Research Documenten/MIMIC-II/V3.0_export/processed/caleb/export_file_caleb.csv', 0)
-
+[attr, values] = c.read_files_and_calculate_attributes('/outcome_result.csv', '/500000_caleb_new.csv', 0)
