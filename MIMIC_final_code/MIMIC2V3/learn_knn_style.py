c2c085973382a0e9f287b0c6c50288a9c5f511cf
alihassouni <ali_e27@hotmail.com>
2016-05-20 10:35:12 +0200

    Update learn_knn_style.py

--- MIMIC_final_code/MIMIC2V3/learn_knn_style.py
+++ MIMIC_final_code/MIMIC2V3/learn_knn_style.py
@@ -1,10 +1,9 @@
 from learning import learnknn
 from util_ import util
 
-#in_dir = '/Users/markhoogendoorn/Documents/Mijn Research Documenten/MIMIC-II/V3.0_export/processed/knn'
-in_dir = 'C:/Users/ali_e_000/Desktop/Research Paper/learn/test-knn'
+in_dir = ''
 
 now = util.get_current_datetime()
-#out_dir = '/Users/markhoogendoorn/Documents/Mijn Research Documenten/MIMIC-II/V3.0_export/learning_results/knn/' + now + '/'
-out_dir = 'C:/Users/ali_e_000/Desktop/Research Paper/' + now + '/'
+
+out_dir = '' + now + '/'
 learnknn.execute_knn(in_dir, out_dir, 'id', 'label', 'day', 1,1)
