257986c1e485e009a6da53db42d5af6ffe0db71d
alihassouni <ali_e27@hotmail.com>
2016-05-20 10:34:38 +0200

    Update learn_caleb_style.py

--- MIMIC_final_code/MIMIC2V3/learn_caleb_style.py
+++ MIMIC_final_code/MIMIC2V3/learn_caleb_style.py
@@ -1,7 +1,7 @@
 from learning import learn
 from util_ import util
 
-in_dir = 'C:/Users/ali_e_000/Desktop/Research Paper/learn/test-lr'
+in_dir = ''
 now = util.get_current_datetime()
-out_dir = 'C:/Users/ali_e_000/Desktop/Research Paper/' + now
+out_dir = '' + now
 learn.execute(in_dir, out_dir, 'id', 'label', 'day', 1, ['LR'], True, False, '')
