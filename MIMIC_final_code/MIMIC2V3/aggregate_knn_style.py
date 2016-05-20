f6ae1c6a6095132b0e854c689d28d3dea4df85f4
alihassouni <ali_e27@hotmail.com>
2016-05-20 10:36:33 +0200

    Update aggregate_knn_style.py

--- MIMIC_final_code/MIMIC2V3/aggregate_knn_style.py
+++ MIMIC_final_code/MIMIC2V3/aggregate_knn_style.py
@@ -3,5 +3,4 @@ from util_ import in_out as io
 
 c = createProcessedFiles()
 
-[attr, values] = c.read_files_and_calculate_attributes('C:/Users/ali_e_000/Desktop/data_mimic_mark/outcome_result.csv', 'C:/Users/ali_e_000/Desktop/Research Paper/knn150k.csv', 2)
-#[attr, values] = c.read_files_and_calculate_attributes('/Users/markhoogendoorn/Documents/Mijn Research Documenten/MIMIC-II/V3.0_export/temp.csv', '/Users/markhoogendoorn/Documents/Mijn Research Documenten/MIMIC-II/V3.0_export/processed/knn/export_file_knn.csv', 2)
+[attr, values] = c.read_files_and_calculate_attributes('/outcome_result.csv', 'data.csv', 2)
